01fb6ff8b9b22b1f804b9e4000cb70870d1ba8d1
Matas Empakeris <matas@getcontrol.co>
2016-02-23 19:14:30 -0800

    [test] calabash sandboxing with travis

--- app/src/main/java/group1/cpsc319/plurilock_client/Presenter/StartActivity.java
+++ app/src/main/java/group1/cpsc319/plurilock_client/Presenter/StartActivity.java
@@ -3,18 +3,29 @@ package group1.cpsc319.plurilock_client.Presenter;
 import android.support.v7.app.AppCompatActivity;
 import group1.cpsc319.plurilock_client.Model.User;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 import group1.cpsc319.plurilock_client.R;
 
 public class StartActivity extends AppCompatActivity {
     private User test;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         test = new User("Matas", "matas@alumni.ubc.ca");
-        TextView tv = (TextView) findViewById(R.id.text_view_1);
+        tv = (TextView) findViewById(R.id.text_view_1);
         tv.setText(test.userMessage());
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               tv = (TextView) findViewById(R.id.text_view_1);
+               tv.setText("You clicked the button!");
+           }
+        });
     }
 }
