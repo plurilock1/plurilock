d68c64131610ef2db87a685e051f57c53cd8cd2e
Baek <baek4055@gmail.com>
2016-02-29 21:02:33 -0800

    Implement displaying X&Y coordinates on Tap events

--- app/src/main/java/group1/cpsc319/plurilock_client/TouchGesture/GestureListener.java
+++ app/src/main/java/group1/cpsc319/plurilock_client/TouchGesture/GestureListener.java
@@ -3,6 +3,7 @@ package group1.cpsc319.plurilock_client.TouchGesture;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.util.Log;
+import android.view.View;
 
 /**
  * Created by BK on 16-02-29.
@@ -15,13 +16,13 @@ public class GestureListener extends GestureDetector.SimpleOnGestureListener
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
-        Log.i(TAG, "Single Tap Up" + getTouchType(e));
+        Log.i(TAG, "Single Tap Up" + coordinate(e));
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
-        Log.i(TAG, "Long Press" + getTouchType(e));
+        Log.i(TAG, "Long Press" + coordinate(e));
     }
 
     @Override
@@ -40,31 +41,38 @@ public class GestureListener extends GestureDetector.SimpleOnGestureListener
 
     @Override
     public void onShowPress(MotionEvent e) {
-        Log.i(TAG, "Show Press" + getTouchType(e));
+        Log.i(TAG, "Show Press" + coordinate(e));
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
-        Log.i(TAG, "Down" + getTouchType(e));
+        Log.i(TAG, "Down" + coordinate(e));
         return false;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
-        Log.i(TAG, "Double tap" + getTouchType(e));
+        Log.i(TAG, "Double tap" + coordinate(e));
         return false;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
-        Log.i(TAG, "Event within double tap" + getTouchType(e));
+        Log.i(TAG, "Event within double tap" + coordinate(e));
         return false;
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
-        Log.i(TAG, "Single tap confirmed" + getTouchType(e));
+        Log.i(TAG, "Single tap confirmed" + coordinate(e));
         return false;
+    }
+
+    private static String coordinate(MotionEvent e) {
+        int x = (int)e.getX();
+        int y = (int)e.getY();
+        String a = "(" + x + " , " + y + ")";
+        return a;
     }
 
     private static String getTouchType(MotionEvent e) {
