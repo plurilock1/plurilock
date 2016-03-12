6007f5f0fa254e109c23b5694db6bcab0f0d1f42
Baek <baek4055@gmail.com>
2016-03-12 13:44:55 -0800

    Implement absolute time and precision

--- app/src/main/java/group1/cpsc319/plurilock_client/TouchGesture/GestureListener.java
+++ app/src/main/java/group1/cpsc319/plurilock_client/TouchGesture/GestureListener.java
@@ -3,7 +3,7 @@ package group1.cpsc319.plurilock_client.TouchGesture;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.util.Log;
-import android.view.View;
+
 
 /**
  * Created by BK on 16-02-29.
@@ -13,69 +13,78 @@ public class GestureListener extends GestureDetector.SimpleOnGestureListener
         implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     public static final String TAG = "GestureListener";
+    public static final String TAG1 = "Duration";
+
+
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
-        Log.i(TAG, "Single Tap Up" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Single Tap Up" + coordination(e) + precision(e) + getTouchType(e));
+        Log.i(TAG1, "Single Tap Up" + abTime(e));
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
-        Log.i(TAG, "Long Press" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Long Press" + coordination(e) + precision(e) + getTouchType(e));
+        Log.i(TAG1, "Long Press" + abTime(e));
+
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
                             float distanceY){
-        Log.i(TAG, "Scroll" + scrollCoordinate(e1, e2) + getTouchType(e1));
+        Log.i(TAG, "Scroll" + scrollCoordination(e1, e2) + getTouchType(e1));
         return false;
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
                            float velocityY) {
-        Log.i(TAG, "Fling" + scrollCoordinate(e1, e2) + getTouchType(e1));
+        Log.i(TAG, "Fling" + scrollCoordination(e1, e2) + getTouchType(e1));
         return false;
     }
 
     @Override
     public void onShowPress(MotionEvent e) {
-        Log.i(TAG, "Show Press" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Show Press" + coordination(e) + precision(e) + getTouchType(e));
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
-        Log.i(TAG, "Down" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Down" + coordination(e) + precision(e) + getTouchType(e));
         return false;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
-        Log.i(TAG, "Double tap" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Double tap" + coordination(e) + precision(e) + getTouchType(e));
+        Log.i(TAG, "Double tap" + abTime(e));
         return false;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
-        Log.i(TAG, "Event within double tap" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Event within double tap" + coordination(e) + precision(e) + getTouchType(e));
+        Log.i(TAG, "Event within double tap" + abTime(e));
         return false;
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
-        Log.i(TAG, "Single tap confirmed" + coordinate(e) + getTouchType(e));
+        Log.i(TAG, "Single tap confirmed" + coordination(e) + precision(e) + getTouchType(e));
+        Log.i(TAG, "Single tap confirmed" + abTime(e));
         return false;
     }
 
-    private static String coordinate(MotionEvent e) {
+    private String coordination(MotionEvent e) {
         int x = (int)e.getX();
         int y = (int)e.getY();
         String a = "(" + x + " , " + y + ")";
         return a;
     }
 
-    private static String scrollCoordinate(MotionEvent e1, MotionEvent e2) {
+    private String scrollCoordination(MotionEvent e1, MotionEvent e2) {
         int x1 = (int)e1.getX();
         int y1 = (int)e1.getY();
         int x2 = (int)e2.getX();
@@ -85,8 +94,25 @@ public class GestureListener extends GestureDetector.SimpleOnGestureListener
         return a;
     }
 
+    private String precision(MotionEvent e){
+        int x = (int)e.getXPrecision();
+        int y = (int)e.getYPrecision();
+        String s =  "(" + x + " , " + y + ")";
+        return s;
+
+    }
+
+    //
+    private String abTime(MotionEvent e) {
+        long eventDuration = e.getEventTime() - e.getDownTime();
+        String time = "(" + eventDuration+ "ms)";
+        return time;
+
+    }
+
 
     private static String getTouchType(MotionEvent e) {
+
 
         String touchTypeDescription = " ";
         int touchType = e.getToolType(0);
